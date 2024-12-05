b719e3ea3ce0836638fe2b70c808b98b87a4a2aa
Sergio Asencio <sergioasenciorodriguez@gmail.com>
2024-12-04 21:40:35 -0500

    Header y Hero Section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderSectionComponent } from "./landing-components/header-section/header-section.component";
+import { HeroSectionComponent } from "./landing-components/hero-section/hero-section.component";
+import { FooterSectionComponent } from "./landing-components/footer-section/footer-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderSectionComponent, HeroSectionComponent, FooterSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
