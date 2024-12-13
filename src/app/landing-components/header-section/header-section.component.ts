a052d14de72f51d268fd291743ed6cc6c2a7b711
Sergio Asencio <sergioasenciorodriguez@gmail.com>
2024-12-13 18:51:10 -0500

    Contacto y precios

--- src/app/landing-components/header-section/header-section.component.ts
+++ src/app/landing-components/header-section/header-section.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header-section',
   standalone: true,
-  imports: [],
   templateUrl: './header-section.component.html',
-  styleUrl: './header-section.component.scss'
+  styleUrls: ['./header-section.component.scss'],
 })
 export class HeaderSectionComponent {
+  activeSection: string = 'hero'; // Sección inicial activa
 
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const sections = document.querySelectorAll('section[id]');
+    let currentSection = '';
+
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const sectionHeight = section.getBoundingClientRect().height;
+
+      if (sectionTop >= 0 && sectionTop <= sectionHeight / 2) {
+        currentSection = section.getAttribute('id') || '';
+      }
+    });
+
+    if (currentSection) {
+      this.activeSection = currentSection;
+      console.log(this.activeSection);
+    }
+  }
+
+  isActive(section: string): boolean {
+    return this.activeSection === section;
+  }
 }
